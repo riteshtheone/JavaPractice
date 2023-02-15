0cbf3d3c5cca9cab9874d953222eb96eff0e9bfe
Ritesh Kumar <riteshnagwan@gmail.com>
2023-02-15 19:12:52 +0530

    leetcode

---
+++ javaPractice/leetcode/PalindromeNumber_9/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isPalindrome(0));
+    }
+}
