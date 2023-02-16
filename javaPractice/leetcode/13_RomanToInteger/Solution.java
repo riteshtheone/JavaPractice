ae28c79be71e5cbb2bd25ba1a6b1c9706ac6b11d
Ritesh Kumar <riteshnagwan@gmail.com>
2023-02-16 20:34:58 +0530

    .

--- javaPractice/leetcode/13_RomanToInteger/Solution.java
+++ javaPractice/leetcode/13_RomanToInteger/Solution.java
@@ -1,11 +1,29 @@
 class Solution {
     public int romanToInt(String s) {
-        return 0;
+        int n, value, prev, result;
+        n = s.length(); value = 0; result = 0;
+        while(--n >= 0){
+            prev = value;
+            value = switch (s.charAt(n)) {
+                case 'I' -> 1;
+                case 'V' -> 5;
+                case 'X' -> 10;
+                case 'L' -> 50;
+                case 'C' -> 100;
+                case 'D' -> 500;
+                case 'M' -> 1000;
+                default -> throw new IllegalArgumentException("Unexpected value: " + s.charAt(n));};
+            // result = (4 * value < result) ? result - value : result + value;
+            // result = (value << 2 < result) ? result - value : result + value;
+            result = (value < prev) ? result - value : result + value;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         var sc = new java.util.Scanner(System.in);
-        
+        String s = sc.next();
+        System.out.println(new Solution().romanToInt(s));
         sc.close();
     }
 }
